8ad638cd99776fc24f7f8d271c396647ea365134
Esther Siebert <esther_siebert@live.nl>
2020-10-01 22:38:58 -0500

    Start on JS

--- main.js
+++ main.js
@@ -1,11 +1,63 @@
+
+/*Variables*/
+
 const btnPrev = document.getElementById("arrow-prev");
 const btnNext = document.getElementById("arrow-next");
 
+const cardItems = document.querySelectorAll("card-item");
+const dots = document.querySelectorAll("dot");
+
+
+const slideNextCard = () => {
+
+}
+
+
+
+btnNext.addEventListener("click", () => {
+    slideNextCard();
+});
+
+
+btnPrev.addEventListener("click", () => {
+    slidePrevCard();
+});
+
+
+
 /*
+
+darkModeButton.addEventListener("click", () => {
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode !== "on") {
+        darkModeOn();
+    } else {
+        darkModeOff();
+    }
+});
+
+const nextCard = () => {
+
+}
+
+event listener 
+
+
 onclick btnNext
 
 loop through array= forEach? 
 
+remove class active-card
+add class active-card
+
+
+
+
+1. 1st of array = active-card
+2. when click on btn next remove active-card class and add it to the next item in the array (array + 1)
+3. animation in css for transition
+4. when clicked btn prev remove active-card class and add it to the previous item (array -1)
+5. According to which item in the array is active, change dot to active?
 
 
 
