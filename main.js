4fc6b43b10798305a0251a6166d69baafccf204e
Esther Siebert <esther_siebert@live.nl>
2020-10-05 23:39:20 +0200

    Both arrows working and pagination dots linked

--- main.js
+++ main.js
@@ -5,14 +5,16 @@ const btnPrev = document.getElementById("arrow-prev");
 const btnNext = document.getElementById("arrow-next");
 
 const cardItems = document.querySelectorAll(".card-item");
-let activeCard = document.querySelector(".active-card");
-let selector = 0;
-
 const dots = document.querySelectorAll(".dot");
 
+let selector = 0;
+
+
+/*Functions*/
 
 const slideNextCard = () => {
-    
+
+    //See if end of cards array is reached    
     if (selector >= cardItems.length - 1) {
 
         //remove arrow or make unclickable
@@ -20,10 +22,17 @@ const slideNextCard = () => {
         return;
 
     } else {
+    
+    //Remove selection
+    cardItems[selector].classList.remove("active-card");
+    dots[selector].classList.remove("active-dot");
 
-    activeCard.classList.remove("active-card");
+    //Increase selector
     selector++;
+
+    //Add next selection
     cardItems[selector].classList.add("active-card");
+    dots[selector].classList.add("active-dot");
     
     }
         
@@ -31,24 +40,32 @@ const slideNextCard = () => {
 
 const slidePrevCard = () => {
 
-    if (selector <= 0) {
+    //See if start of cards array is reached
+    if (selector === 0) {
 
         //remove arrow or make unclickable
 
         return;
 
     } else {
+    
+    //Remove current selection
+    cardItems[selector].classList.remove("active-card");
+    dots[selector].classList.remove("active-dot");
 
-    activeCard.classList.remove("active-card");
-
+    //Decrease selector
     selector--;
 
+    //Add previous selection
     cardItems[selector].classList.add("active-card");
+    dots[selector].classList.add("active-dot");
 
     }
 
 };
 
+
+/*Event listeners*/
 
 btnNext.addEventListener("click", () => {
     slideNextCard();
@@ -61,45 +78,3 @@ btnPrev.addEventListener("click", () => {
 
 
 
-
-
-
-/*
-document.body.classList.remove("darkmode");
-
-
-darkModeButton.addEventListener("click", () => {
-    darkMode = localStorage.getItem("darkMode");
-    if (darkMode !== "on") {
-        darkModeOn();
-    } else {
-        darkModeOff();
-    }
-});
-
-const nextCard = () => {
-
-}
-
-event listener 
-
-
-onclick btnNext
-
-loop through array= forEach? 
-
-remove class active-card
-add class active-card
-
-
-
-
-1. 1st of array = active-card
-2. when click on btn next remove active-card class and add it to the next item in the array (array + 1)
-3. animation in css for transition
-4. when clicked btn prev remove active-card class and add it to the previous item (array -1)
-5. According to which item in the array is active, change dot to active?
-
-
-
-*/
